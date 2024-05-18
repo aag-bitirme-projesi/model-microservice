37f76f3284bcfec20d5d98a6e9e10d35552775a3
ayca <aycaakyol3@gmail.com>
2024-05-18 13:36:32 +0300

    merged upload-dataset

--- utils/storage.py
+++ utils/storage.py
@@ -50,7 +50,7 @@ class StorageService:
 def get_s3_ayca():
     return boto3.client(
         's3',
-        aws_access_key_id='AKIAZS6EJ2S5MPKFEV4B',
-        aws_secret_access_key='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',
-        region_name='us-east-1',
+        aws_access_key_id=os.getenv('AYCA_AWS_ACCESS_KEY'),
+        aws_secret_access_key=os.getenv('AYCA_AWS_ACCESS_KEY'),
+        region_name=os.getenv('AWS_ACCESS_KEY'),
     )
